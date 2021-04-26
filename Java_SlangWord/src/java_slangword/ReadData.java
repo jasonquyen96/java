386e147d63d46479a42e51ed03acc70fd8e6bfb2
jasonquyen96 <32591595+jasonquyen96@users.noreply.github.com>
2021-04-26 16:21:05 +0700

    add class ReadData

---
+++ Java_SlangWord/src/java_slangword/ReadData.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_slangword;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Admin
+ */
+public class ReadData {
+    private static final String FILENAME = "slang.txt";
+    
+        public Object read() {
+        var data = new ArrayList<>();
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+        return null;
+    }
+}
