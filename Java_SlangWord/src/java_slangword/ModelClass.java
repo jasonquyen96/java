451baa20d5efcce53e02b4885bf295da941616de
jasonquyen96 <32591595+jasonquyen96@users.noreply.github.com>
2021-04-27 13:24:41 +0700

    edit code

---
+++ Java_SlangWord/src/java_slangword/ModelClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_slangword;
+
+/**
+ *
+ * @author Admin
+ */
+public class ModelClass {
+    public int STT;
+    public String Slang;
+}
